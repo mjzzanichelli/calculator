19325d3a758a252e488e494eda3447d49374717a
José Marco Zanichelli <mjzzanichelli@hotmail.com>
2022-01-10 12:32:58 +0100

    fixed duplication of scripts by tweeking the webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, "assets", "index.html"),
+      filename: path.join(__dirname, "public", "index.html"),
     }),
   ],
 };
